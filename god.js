0defb2ca1edd6c490a7084f4e2d807bd758d50b3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-09-03 23:59:58 +0700

    test

--- god.js
+++ god.js
@@ -8,8 +8,7 @@ function padLeadingZeros(num, size) {
     return s;
 }
 
-async function start() {
-let year = 2533;
+/*let year = 2533;
 let preyearlist = [];
 let preyearsuperlist = [];
 let yearlist = [];
@@ -96,17 +95,122 @@ while (year <= nextyear) {
                     fs.writeFile('tmp/' + day[3] + '.txt', JSON.stringify(preyearlist), function (err) {
                         if (err) throw err;
                     });*/
-                }
-            })
-    }
-    year += 10
+/*}
+})
 }
+year += 10
+}*/
 /*fs.writeFile('tmp/cache.txt', JSON.stringify(yearlist), function (err) {
     if (err) throw err;
     res.send(yearlist)
 });*/
-fs.writeFile('god', JSON.stringify(yearlist), function (err) {
+/*fs.writeFile('god', JSON.stringify(yearlist), function (err) {
     if (err) throw err;
     res.send(yearlist)
-});
+});*/
+
+async function get_request() {
+    let year = 2533;
+    let preyearlist = [];
+    let preyearsuperlist = [];
+    let yearlist = [];
+    let nextyear = new Date().getFullYear() + 543;
+    let channel = [];
+    //let jdata
+    let countloveme = 0
+    var fileContents = null;
+    try {
+        //fileContents = fs.readFileSync('tmp/cache.txt');
+        fileContents = fs.readFileSync('god');
+    } catch (err) {
+    }
+    if (fileContents) {
+        yearlist = JSON.parse(fileContents);
+        if (
+            yearlist[yearlist.length - 1].substring(4, 8) ==
+            new Date().getFullYear() + 543
+        ) {
+            year = new Date().getFullYear() + 543;
+        } else {
+            year = yearlist[yearlist.length - 1].substring(4, 8)
+        }
+        yearlist.forEach(function (value, i) {
+            if (
+                value.substring(4, 8) ==
+                year
+            ) {
+                countloveme--;
+            }
+        });
+        yearlist.splice(countloveme);
+    }
+    let day
+    while (year <= nextyear) {
+        channel = []
+        for (let i = 0; i < 10; i++) {
+            preyearsuperlist = [];
+            preyearlist = [];
+            let peryear = [];
+            let ayear = year + i
+            if (ayear > nextyear) {
+                break
+            }
+            //await fetch('https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-' + ayear + '.aspx')
+            //    .then(res => res.text())
+            //    .then((body) => {
+            const url = 'https://www.myhora.com/%E0%B8%AB%E0%B8%A7%E0%B8%A2/%E0%B8%9B%E0%B8%B5-' + ayear + '.aspx'
+            const res = await fetch(url);
+            const body = await res.text();
+            var $ = cheerio.load(body);
+            for (const val of $('font').toArray()) {
+                if (val.firstChild.data.indexOf('ตรวจสลากกินแบ่งรัฐบาล') > -1) {
+                    day = val.firstChild.data.split(" ").splice(2)
+                    let monthnum
+                    switch (day[2]) {
+                        case 'มกราคม': monthnum = "01"; break;
+                        case 'กุมภาพันธ์': monthnum = "02"; break;
+                        case 'มีนาคม': monthnum = "03"; break;
+                        case 'เมษายน': monthnum = "04"; break;
+                        case 'พฤษภาคม': monthnum = "05"; break;
+                        case 'มิถุนายน': monthnum = "06"; break;
+                        case 'กรกฎาคม': monthnum = "07"; break;
+                        case 'สิงหาคม': monthnum = "08"; break;
+                        case 'กันยายน': monthnum = "09"; break;
+                        case 'ตุลาคม': monthnum = "10"; break;
+                        case 'พฤศจิกายน': monthnum = "11"; break;
+                        case 'ธันวาคม': monthnum = "12"; break;
+                    }
+                    peryear.unshift(padLeadingZeros(day[0], 2) + monthnum + day[3])
+                    preyearsuperlist.unshift(padLeadingZeros(day[0], 2) + monthnum + day[3])
+                }
+            }
+            for (const val of peryear) {
+                yearlist.push(val)
+            }
+            for (const val of preyearsuperlist) {
+                preyearlist.push(val)
+                /*try {
+                    if (day[3] == new Date().getFullYear() + 543) {
+                        fs.unlinkSync('tmp/' + req.query.date + '.txt');
+                        console.log('yes this year')
+                    }
+                } catch (err) {
+ 
+                }
+                fs.writeFile('tmp/' + day[3] + '.txt', JSON.stringify(preyearlist), function (err) {
+                    if (err) throw err;
+                });*/
+            }
+            //})
+        }
+        year += 10
+    }
+    /*fs.writeFile('tmp/cache.txt', JSON.stringify(yearlist), function (err) {
+        if (err) throw err;
+        res.send(yearlist)
+    });*/
+    fs.writeFile('god', JSON.stringify(yearlist), function (err) {
+        if (err) throw err;
+        res.send(yearlist)
+    });
 }
