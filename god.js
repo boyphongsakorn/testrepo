bf3eaa2342b08126b8f46bc61f145e9421eea9c3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-09-27 02:22:24 +0700

    ok

--- god.js
+++ god.js
@@ -165,12 +165,12 @@ get_request()
 get_zero()*/
 
 async function get_one() {
-    for (let snum = 0; snum < 100; snum++) {
+    for (let snum = 0; snum < 1000; snum++) {
 
         await fetch('https://lotapi.pwisetthon.com/finddol?search=' + padLeadingZeros(snum, 6), { "method": "GET" })
             .then(res => res.json())
             .then((body) => {
-                fs.writeFile(padLeadingZeros(snum, 6), JSON.stringify(body), function (err) {
+                fs.writeFile("tmp/"+padLeadingZeros(snum, 6), JSON.stringify(body), function (err) {
                     if (err) throw err;
                     //res.send(yearlist)
                 });
