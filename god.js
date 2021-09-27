0dde8791a81b3c633e965ce126949197b3f8c27b
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2021-09-27 16:54:49 +0700

    Update god.js

--- god.js
+++ god.js
@@ -163,21 +163,3 @@ get_request()
 }
 
 get_zero()*/
-
-async function get_one() {
-    for (let snum = 0; snum < 10000; snum++) {
-
-        await fetch('https://lotapi.pwisetthon.com/finddol?search=' + padLeadingZeros(snum, 6), { "method": "GET" })
-            .then(res => res.json())
-            .then((body) => {
-                if (body.length != 0) {
-                    fs.writeFile("tmp/" + padLeadingZeros(snum, 6), JSON.stringify(body), function (err) {
-                        if (err) throw err;
-                        //res.send(yearlist)
-                    });
-                }
-            });
-    }
-}
-
-get_one()
