1f6f02d286a659e722d1c73f96c80a9cff3f56fb
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-09-28 20:56:41 +0700

    test

---
+++ partfive.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio')
+var fs = require('fs')
+
+function padLeadingZeros(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+async function get_one() {
+    for (let snum = 10000; snum < 12500; snum++) {
+
+        await fetch('https://lotapi.pwisetthon.com/finddol?search=' + padLeadingZeros(snum, 6), { "method": "GET" })
+            .then(res => res.json())
+            .then((body) => {
+                if (body.length != 0) {
+                    fs.writeFile("tmp/" + padLeadingZeros(snum, 6), JSON.stringify(body), function (err) {
+                        if (err) throw err;
+                        //res.send(yearlist)
+                    });
+                }
+            });
+    }
+}
+
+get_one()
