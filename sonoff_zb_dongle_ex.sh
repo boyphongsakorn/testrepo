ba98fa6196c284d179df21579ad8d1e5679aa4f5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-06-23 19:56:38 +0700

    test

--- sonoff_zb_dongle_ex.sh
+++ sonoff_zb_dongle_ex.sh
@@ -1,3 +1,4 @@
+docker stop $(docker ps -q -f "name=zigbee2mqtt")
 git clone https://github.com/JelmerT/cc2538-bsl.git
 pip3 install pyserial 
 pip3 install intelhex
@@ -6,3 +7,4 @@ wget {{sonoff_zb_dongle_url}}
 unzip {{donglezipfile}}
 cd cc2538-bsl
 ./cc2538-bsl.py -p /dev/ttyUSB0 -e -v -w --bootloader-sonoff-usb ~/{{donglezipfile}}.hex
+docker start $(docker ps -a -q -f "name=zigbee2mqtt")
