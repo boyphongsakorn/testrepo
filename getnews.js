e57f3c4b95e3747db9090b67755580e8ef4e57ad
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-15 08:43:50 +0700

    ok

---
+++ getnews.js
@@ -0,0 +1,16 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+var fs = require('fs')
+
+async function get_one() {
+    await fetch('https://lotapi.pwisetthon.com/lotnews?count=40')
+            .then(res => res.json())
+            .then((body) => {
+                //res.send(allwin)
+                fs.writeFile("latestnews.json", JSON.stringify(body), function (err) {
+                    if (err) throw err;
+                    //res.send(yearlist)
+                });
+            })
+}
+
+get_one()
