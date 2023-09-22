77ea8c341efee4a194051739b95164e65f129294
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-09-22 10:02:08 +0000

    not 5xx

--- getnews.js
+++ getnews.js
@@ -1,16 +1,18 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 var fs = require('fs')
 
 async function get_one() {
     await fetch('https://lotapi.pwisetthon.com/lotnews?count=40')
-            .then(res => res.json())
-            .then((body) => {
-                //res.send(allwin)
+        .then(res => res.json())
+        .then((body) => {
+            //res.send(allwin)
+            if (body.status != 500) {
                 fs.writeFile("latestnews.json", JSON.stringify(body), function (err) {
                     if (err) throw err;
                     //res.send(yearlist)
                 });
-            })
+            }
+        })
 }
 
 get_one()
--- so_zb_de_updatescript.js
+++ so_zb_de_updatescript.js
@@ -1,36 +1,40 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const fs = require('fs');
 
 (async () => {
-    const api = await fetch('https://api.github.com/repos/Koenkk/Z-Stack-firmware/git/trees/master?recursive=1')
-    const json = await api.json()
-    const tree = json.tree
-    //find path coordinator/Z-Stack_3.x.0/bin in the tree
-    const path = tree.find(({path}) => path === 'coordinator/Z-Stack_3.x.0/bin')
-    //get url of the blob
-    const url = path.url
-    console.log(url)
-    const blob = await fetch(url)
-    const ajson = await blob.json()
-    const atree = ajson.tree
-    //find word launchpad_coordinator in paths of the tree
-    const path2 = atree.find(({path}) => path.includes('launchpad_coordinator'))
-    //get path of the blob
-    const url2 = path2.path
-    const downloadilnk = 'https://raw.githubusercontent.com/Koenkk/Z-Stack-firmware/master/'+path.path+'/'+url2
-    console.log(downloadilnk)
-    //read sonoff_zb_dongle_ex.sh file
-    const sonoff_zb_dongle_ex = fs.readFileSync('sonoff_zb_dongle_ex.sh', 'utf8')
-    //replace {{sonoff_zb_dongle_url}} with download link in sonoff_zb_dongle_ex.sh
-    const sonoff_zb_dongle_ex_new = sonoff_zb_dongle_ex.replace('{{sonoff_zb_dongle_url}}', downloadilnk)
-    //replace all {{donglezipfile}} with url2 in sonoff_zb_dongle_ex.sh
-    const sonoff_zb_dongle_ex_new2 = sonoff_zb_dongle_ex_new.replace('{{donglezipfile}}', url2)
-    //const sonoff_zb_dongle_ex_new3 = sonoff_zb_dongle_ex_new2.replace('{{donglezipfile}}', url2)
-    //remove .zip from url2
-    const url2withoutzip = url2.replace('.zip', '')
-    const sonoff_zb_dongle_ex_new4 = sonoff_zb_dongle_ex_new2.replace('{{donglezipfile}}', url2withoutzip)
-    const sonoff_zb_dongle_ex_new5 = sonoff_zb_dongle_ex_new4.replace('{{donglezipfile}}', url2withoutzip)
-    const sonoff_zb_dongle_ex_new6 = sonoff_zb_dongle_ex_new5.replace('{{donglezipfile}}', url2withoutzip)
-    //write sonoff_zb_dongle_ex.sh file
-    fs.writeFileSync('sonoff_zb_dongle.sh', sonoff_zb_dongle_ex_new6)
+    try {
+        const api = await fetch('https://api.github.com/repos/Koenkk/Z-Stack-firmware/git/trees/master?recursive=1')
+        const json = await api.json()
+        const tree = json.tree
+        //find path coordinator/Z-Stack_3.x.0/bin in the tree
+        const path = tree.find(({ path }) => path === 'coordinator/Z-Stack_3.x.0/bin')
+        //get url of the blob
+        const url = path.url
+        console.log(url)
+        const blob = await fetch(url)
+        const ajson = await blob.json()
+        const atree = ajson.tree
+        //find word launchpad_coordinator in paths of the tree
+        const path2 = atree.find(({ path }) => path.includes('launchpad_coordinator'))
+        //get path of the blob
+        const url2 = path2.path
+        const downloadilnk = 'https://raw.githubusercontent.com/Koenkk/Z-Stack-firmware/master/' + path.path + '/' + url2
+        console.log(downloadilnk)
+        //read sonoff_zb_dongle_ex.sh file
+        const sonoff_zb_dongle_ex = fs.readFileSync('sonoff_zb_dongle_ex.sh', 'utf8')
+        //replace {{sonoff_zb_dongle_url}} with download link in sonoff_zb_dongle_ex.sh
+        const sonoff_zb_dongle_ex_new = sonoff_zb_dongle_ex.replace('{{sonoff_zb_dongle_url}}', downloadilnk)
+        //replace all {{donglezipfile}} with url2 in sonoff_zb_dongle_ex.sh
+        const sonoff_zb_dongle_ex_new2 = sonoff_zb_dongle_ex_new.replace('{{donglezipfile}}', url2)
+        //const sonoff_zb_dongle_ex_new3 = sonoff_zb_dongle_ex_new2.replace('{{donglezipfile}}', url2)
+        //remove .zip from url2
+        const url2withoutzip = url2.replace('.zip', '')
+        const sonoff_zb_dongle_ex_new4 = sonoff_zb_dongle_ex_new2.replace('{{donglezipfile}}', url2withoutzip)
+        const sonoff_zb_dongle_ex_new5 = sonoff_zb_dongle_ex_new4.replace('{{donglezipfile}}', url2withoutzip)
+        const sonoff_zb_dongle_ex_new6 = sonoff_zb_dongle_ex_new5.replace('{{donglezipfile}}', url2withoutzip)
+        //write sonoff_zb_dongle_ex.sh file
+        fs.writeFileSync('sonoff_zb_dongle.sh', sonoff_zb_dongle_ex_new6)
+    } catch (error) {
+        console.log('ok error but continue')
+    }
 })()
